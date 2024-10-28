36d58959a3a47c8690e907dc8dc960c231a52f17
Emily Knox-Clifton <128257305+eknox22@users.noreply.github.com>
2024-10-28 21:13:56 +0000

    Add logic to organise population data by continent and year
    - Check if each continent exists in the population_per_continent dictionary.
    - If a continent is missing, initialise it with empty lists for population and years.
    - Append population and year data to the lists for each respective continent.
    - This update allows population data to be structured by continent and year, enabling easier data access and plotting.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 filename = 'data.csv'
 
+population_per_continent = {}
+
 with open(filename, 'r') as csvfile:
   reader = csv.DictReader(csvfile)
   for line in reader:
@@ -10,14 +12,29 @@ with open(filename, 'r') as csvfile:
     year = line['year']
     population = line['population']
 
+    # Print out the data from each row of the CSV file
+
     print(continent)
     print(year)
     print(population)
 
+    # Check if the continent is already a key in the dictionary
+
+    if continent not in population_per_continent:
+      population_per_continent[continent] = {'population': [], 'years': []}
+
+     # Append the population and year data to the appropriate lists for each continent
+    population_per_continent[continent]['population'].append(population)
+    population_per_continent[continent]['years'].append(year)
 
 
-plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400], label="Europe", marker='o')
-plt.plot([2000, 2010, 2020, 2030], [200, 600, 400, 100], label="Asia", marker='o')
+
+plt.plot([2000, 2010, 2020, 2030], [100, 200, 300, 400],
+         label="Europe",
+         marker='o')
+plt.plot([2000, 2010, 2020, 2030], [200, 600, 400, 100],
+         label="Asia",
+         marker='o')
 
 plt.title('Internet Population per continent')
 plt.xlabel('Year')
@@ -26,3 +43,4 @@ plt.grid(True)
 plt.legend()
 
 plt.show()
+
